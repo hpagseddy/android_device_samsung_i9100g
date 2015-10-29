80430f4c3a4a4ce598980cbd51e47a86dfd1d1b0
xdaamg <goetz@gametool.de>
2015-10-29 15:31:17 +0100

    Update i9100g.mk
    remove Torch from PRODUCT_PACKAGES

--- i9100g.mk
+++ i9100g.mk
@@ -63,9 +63,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     $(LOCAL_PATH)/configs/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml
 
 # Packages
 PRODUCT_PACKAGES += \
@@ -74,8 +74,7 @@ PRODUCT_PACKAGES += \
     hwcomposer.t1 \
     lights.t1 \
     power.t1 \
-    SamsungServiceMode \
-    Torch
+    SamsungServiceMode
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
