886698f4f61f51154f27fde0453adb9c5447f172
xdaamg <goetz@gametool.de>
2015-12-08 13:15:20 +0100

    replace cyanogen recovery with twrp
    Change-Id: I332685b70b116bb81a94024183770242804125e3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -141,6 +141,11 @@ TARGET_OTA_ASSERT_DEVICE := i9100g,GT-I9100G
 # device-specific extensions to the updater binary
 TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/i9100g
 
+# TWRP
+TW_THEME := portrait_mdpi
+TW_HAS_NO_RECOVERY_PARTITION := true
+TW_EXCLUDE_ENCRYPTED_BACKUPS := true
+
 # Releasetools
 TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./device/samsung/i9100g/releasetools/t1_ota_from_target_files
 TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/i9100g/releasetools/t1_img_from_target_files
