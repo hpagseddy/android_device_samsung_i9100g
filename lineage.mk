18e985ae6ff5994d4a2c09ab10da73012a9e42a7
xdaamg <goetz@gametool.de>
2017-06-09 20:55:43 +0200

    update liblights based on hardware/samsung; add overlay display settings
    Change-Id: Ifd4dbf595d7947724624edc4800f06b65ff69ed3

--- lineage.mk
+++ lineage.mk
@@ -17,6 +17,10 @@
 # Release name
 PRODUCT_RELEASE_NAME := GT-I9100G
 
+# Bootanimation
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
