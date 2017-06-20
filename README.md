1139338aff108c997e55030b794540136cb8dd1c
xdaamg <goetz@gametool.de>
2017-06-20 13:03:34 +0200

    reset lineage 13.0 201706 (2)
    Change-Id: Idb09544e9611632c9cd1b5ced0771d35b42034dc

--- README.md
+++
@@ -1,2 +0,0 @@
-# android_device_samsung_i9100g
-Samsung Galaxy S II (GT-I9100G)
--- shims/Android.mk
+++
@@ -1,5 +0,0 @@
-# Copyright (C) 2016 The LineageOS Project
-
-LOCAL_PATH := $(call my-dir)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
